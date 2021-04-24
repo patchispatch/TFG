abcd5b8d95edd483379a4db3ea968ae3b24d5928
Juan Ocaña <juanov98@gmail.com>
2021-04-24 19:57:08 +0200

    Add Swagger UI via drf_yasg

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,13 +1,35 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from core import views
+
 
 router = DefaultRouter()
 router.register(r'activities', views.ActivityViewSet)
 router.register(r'objectives', views.ObjectiveViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="TFG API",
+        default_version='v1',
+        description="API para la aplicación propuesta como TFG",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
+    # Swagger UI
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    # Admin site
     path('admin/', admin.site.urls),
+
+    # Root
     path('', include(router.urls)),
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,16 +1,54 @@
+from django.utils.decorators import method_decorator
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework import viewsets
+from drf_yasg.utils import swagger_auto_schema
 from .utils import EnablePartialUpdateMixin
 from .models import *
 from .serializers import *
 
 # Create your views here:
-
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    operation_description="Get a list of all activities"
+))
+@method_decorator(name='retrieve', decorator=swagger_auto_schema(
+    operation_description="Get an activity by ID"
+))
+@method_decorator(name='create', decorator=swagger_auto_schema(
+    operation_description="Create a new activity"
+))
+@method_decorator(name='update', decorator=swagger_auto_schema(
+    operation_description="Update an existing activity"
+))
+@method_decorator(name='partial_update', decorator=swagger_auto_schema(
+    operation_description="Partially update an existing activity"
+))
+@method_decorator(name='destroy', decorator=swagger_auto_schema(
+    operation_description="Delete an activity by ID"
+))
 class ActivityViewSet(viewsets.ModelViewSet):
     queryset = Activity.objects.all()
     serializer_class = ActivitySerializer
 
+
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    operation_description="Get a list of all objectives"
+))
+@method_decorator(name='retrieve', decorator=swagger_auto_schema(
+    operation_description="Get an objective by ID"
+))
+@method_decorator(name='create', decorator=swagger_auto_schema(
+    operation_description="Create a new objective"
+))
+@method_decorator(name='update', decorator=swagger_auto_schema(
+    operation_description="Update an existing objective"
+))
+@method_decorator(name='partial_update', decorator=swagger_auto_schema(
+    operation_description="Partially update an existing objective"
+))
+@method_decorator(name='destroy', decorator=swagger_auto_schema(
+    operation_description="Delete an objective by ID"
+))
 class ObjectiveViewSet(viewsets.ModelViewSet):
     queryset = Objective.objects.all()
     serializer_class = ObjectiveSerializer
