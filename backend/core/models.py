37dbc61670ffa6f1a73693b84e96b275404e6650
Juan Ocaña <juanov98@gmail.com>
2021-07-13 18:49:19 +0200

    Add current streak property to Objective

--- backend/core/models.py
+++ backend/core/models.py
@@ -29,7 +29,38 @@ class Objective(models.Model):
         # Calculate dates
         last_date = today + timedelta((reset_day - today.weekday()) % 7)
         first_date = last_date - timedelta(7)
+        
+        return self.__progress_between_dates(first_date, last_date)
+    
+    @property
+    def current_streak(self):
+        streak = 0
+        completed = True if self.progress >= self.goal else False
+        reset_day = Settings.objects.filter().first().weekly_reset_day
+        reset_day = date.today() + timedelta((reset_day - date.today().weekday()) % 7)
 
+        while completed:
+            streak += 1
+
+            # Check past week
+            reset_day = reset_day - timedelta(7)
+            first_dow = reset_day - timedelta(7)
+            completed = True if self.__progress_between_dates(first_dow, reset_day) >= self.goal else False
+        
+        return streak
+    
+    def __str__(self):
+        return self.name
+
+    def pause_resume(self):
+        self.paused = not self.paused
+        self.save()
+
+    
+    def __progress_between_dates(self, first_date, last_date):
+        """
+        Returns the progress of the current objective between two dates
+        """
         entries = ObjectiveEntry.objects.select_related('objective_id').filter(
             objective_id=self.id,
             date__gte=first_date,
@@ -41,18 +72,7 @@ class Objective(models.Model):
             progress += entry.progress
         
         return progress
-    
-    @property
-    def current_streak(self):
-        # TODO: implement
-        pass
-    
-    def __str__(self):
-        return self.name
 
-    def pause_resume(self):
-        self.paused = not self.paused
-        self.save()
 
 
 class ObjectiveEntry(models.Model):
